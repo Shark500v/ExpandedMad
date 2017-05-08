c782b1df44e94ca03b180b8caf34429270c51584
Alessio <ale.sacco14@gmail.com>
2017-05-08 22:20:58 +0200

    Nuova classe per chat

---
+++ app/src/main/java/com/polito/madinblack/expandedmad/model/Message.java
@@ -0,0 +1,37 @@
+package com.polito.madinblack.expandedmad.model;
+
+import java.util.Date;
+
+public class Message {
+
+    private String sentBy;
+    private String message;
+    private Date date;
+
+    public Message() {
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
