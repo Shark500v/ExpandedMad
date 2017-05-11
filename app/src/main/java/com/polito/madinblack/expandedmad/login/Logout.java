70561172936a92a8362c24943c32c4d271ea0d45
Ale <alessandro.nc8@gmail.com>
2017-05-11 23:52:25 +0200

    Modify something

--- app/src/main/java/com/polito/madinblack/expandedmad/login/Logout.java
+++ app/src/main/java/com/polito/madinblack/expandedmad/login/Logout.java
@@ -2,14 +2,26 @@ package com.polito.madinblack.expandedmad.login;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
 import com.polito.madinblack.expandedmad.R;
 
 
-public class Logout extends DialogFragment {
+public class Logout extends DialogFragment  {
+
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
@@ -17,12 +29,39 @@ public class Logout extends DialogFragment {
         AlertDialog alertDialog;
 
 
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         builder.setTitle(getString(R.string.sure));
         // Add the buttons
         builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // User clicked OK button
                 //manager here the logout
+
+                    // Firebase sign out
+                    mAuth.signOut();
+
+                    // Google sign out
+                    Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                            new ResultCallback<Status>() {
+                                @Override
+                                public void onResult(@NonNull Status status) {
+                                    Intent intent = new Intent(getActivity(), CheckLogIn.class);
+                                    startActivity(intent);
+
+                                }
+                            });
+
 
             }
         });
