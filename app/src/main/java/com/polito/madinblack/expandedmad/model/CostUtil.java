0645ea1b9275ff4f988ce14cdeffc7654c4d2041
Alessio <ale.sacco14@gmail.com>
2017-05-11 17:29:07 +0200

    Sistemato tracking

--- app/src/main/java/com/polito/madinblack/expandedmad/model/CostUtil.java
+++ app/src/main/java/com/polito/madinblack/expandedmad/model/CostUtil.java
@@ -10,7 +10,8 @@ public class CostUtil {
         for (int i = 1; i < scale; i++)
             pow *= 10;
         double tmp = number * pow;
-        return (double) (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) / pow;
+        //return (double) (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) / pow;
+        return (double) (int)( tmp + 1 ) / pow;
     }
 
     public static boolean validateTelFaxNumber(String number)
