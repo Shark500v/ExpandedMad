3b140c8f3fc5e2e404effbd4f32212b92c53eca5
Ale <alessandro.nc8@gmail.com>
2017-05-15 16:15:38 +0200

    Modify something

--- app/src/main/java/com/polito/madinblack/expandedmad/model/Currency.java
+++ app/src/main/java/com/polito/madinblack/expandedmad/model/Currency.java
@@ -1,13 +1,11 @@
 package com.polito.madinblack.expandedmad.model;
 
 
-import android.app.Activity;
-import android.content.Context;
-
-import com.bumptech.glide.load.engine.Resource;
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Ale on 27/04/2017.
@@ -15,52 +13,78 @@ import java.util.Map;
 
 public class Currency {
 
-    double [][] association = new double[4][4];
+    private static final double [][] association = new double[4][4];
 
-    public static final Map<String, Integer> currencySymbolInt = new HashMap<String, Integer>() {{
-        put("€", 0);
-        put("$", 1);
-        put("¥", 2);
-        put("£", 3);
+    static {
+        association[0][0] = 1;
+        association[1][1] = 1;
+        association[2][2] = 1;
+        association[3][3] = 1;
+        association[0][1] = 0.91084;
+        association[0][2] = 0.77395;
+        association[0][3] = 113.354;
+        association[1][0] = 1.09789;
+        association[1][2] = 0.84971;
+        association[1][3] = 124.450;
+        association[2][0] = 0.00882;
+        association[2][1] = 0.00804;
+        association[2][3] = 0.00683;
+    }
+
+
+
+    private static final Map<String, Integer> currencySymbolInt = new HashMap<String, Integer>() {{
+        put("$", 0);
+        put("€", 1);
+        put("¥", 3);
+        put("£", 2);
     }};
 
-    public static final Map<String, String> currencyNameSymbol = new HashMap<String, String>() {{
-        put("Euro", "€");
-        put("Dollar", "$");
-        put("Yen",  "¥");
-        put("Pound",  "£");
+    private static final Map<String, String> currencyNameSymbol = new HashMap<String, String>() {{
+        put("USD", "$");
+        put("EUR", "€");
+        put("GBP",  "£");
+        put("JPY",  "¥");
     }};
-    public static final Map<String, String> currencyNameSymbol = new HashMap<String, String>() {{
-        put("Euro", "€");
-        put("Dollar", "$");
-        put("Yen",  "¥");
-        put("Pound",  "£");
-    }};
-
-
-
-    public static final Map<String, String> currencyNameSymbol = new HashMap<String, String>() {{
-        put("Euro", "€");
-        put("Dollar", "$");
-        put("Yen",  "¥");
-        put("Pound",  "£");
-    }};
-
-
-    public static final Map<String, String> currencyNameValue = new HashMap<String, String>() {{
-        put("Euro", "€");
-        put("Dollar", "$");
-        put("Yen",  "¥");
-        put("Pound",  "£");
+    private static final Map<String, String> currencySymbolName = new HashMap<String, String>() {{
+        put("$", "USD");
+        put("€", "EUR");
+        put("£",  "GBP");
+        put("¥",  "JPY");
     }};
 
 
 
-    public Currency(){
+    public static double convertCurrency(Double quantity, String currency1, String currency2){
+
+        if(currencySymbolInt.containsKey(currency1) && currencySymbolInt.containsKey(currency2)){
+            double resultToRound = quantity * association[currencySymbolInt.get(currency1)][currencySymbolInt.get(currency1)];
+            return new BigDecimal(resultToRound).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        }
+        else
+            return -1;
 
     }
 
-    public static Map<String, String> getCurrency() {
-        return currency;
+    public static String getSymbol(String name){
+        return currencyNameSymbol.get(name);
+
     }
+
+    public static String getName(String symbol){
+        return currencySymbolName.get(symbol);
+
+    }
+
+    public static Set<String> getCurrencyNames(){
+        return currencyNameSymbol.keySet();
+    }
+
+    public static Set<String> getCurrencySymbols(){
+        return currencySymbolName.keySet();
+    }
+
+
+
+
 }
