b01d707c704e3743bf8a4e7e57dc51bc139e6969
Shark500v <francesco.pipito@gmail.com>
2017-05-23 21:10:08 +0200

    final update setting language

---
+++ app/src/main/java/com/polito/madinblack/expandedmad/AppMain.java
@@ -0,0 +1,41 @@
+package com.polito.madinblack.expandedmad;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
+
+public class AppMain extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String LangValue = preferences.getString("pref_key_language", "error");
+
+        if (LangValue.compareTo("1") == 0){
+            setLocale("it");
+        }else {
+            setLocale("en");
+        }
+    }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+    }
+}
