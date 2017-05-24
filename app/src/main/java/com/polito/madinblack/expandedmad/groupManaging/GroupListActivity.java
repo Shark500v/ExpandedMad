684980ab901e86374b098e510c6585f4638eb983
Shark500v <francesco.pipito@gmail.com>
2017-05-24 12:54:18 +0200

    search button update grouplistActivity

--- app/src/main/java/com/polito/madinblack/expandedmad/groupManaging/GroupListActivity.java
+++ app/src/main/java/com/polito/madinblack/expandedmad/groupManaging/GroupListActivity.java
@@ -37,6 +37,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.polito.madinblack.expandedmad.R;
+import com.polito.madinblack.expandedmad.newGroup.SelectUser;
 import com.polito.madinblack.expandedmad.settings.*;
 import com.polito.madinblack.expandedmad.StatisticsGraphs;
 import com.polito.madinblack.expandedmad.UserPage;
@@ -50,6 +51,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -222,7 +224,19 @@ public class GroupListActivity extends AppCompatActivity implements NavigationVi
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
 
-        // Configure the search info and add any event listeners...
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.filter(newText);
+                return false;
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -248,14 +262,14 @@ public class GroupListActivity extends AppCompatActivity implements NavigationVi
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     mValues.clear();
+                    duplicato.clear();
                     for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
 
                         mValues.add(postSnapshot.getValue(GroupForUser.class));
+                        duplicato.add(postSnapshot.getValue(GroupForUser.class));
 
                     }
                     notifyDataSetChanged();
-
-
                 }
 
                 @Override
@@ -266,7 +280,7 @@ public class GroupListActivity extends AppCompatActivity implements NavigationVi
                         Toast.makeText(mContext, getString(R.string.fail_load_group),
                                 Toast.LENGTH_SHORT).show();
                     }
-                    }
+                }
             };
             if(ref!=null)
                 ref.addValueEventListener(eventListener);
@@ -396,6 +410,23 @@ public class GroupListActivity extends AppCompatActivity implements NavigationVi
                 return super.toString();
             }
         }
-    }
 
+        // Filter Class
+        public void filter(String charText) {
+            charText = charText.toLowerCase(Locale.getDefault());
+            mValues.clear();
+            if (charText.length() == 0) {
+                mValues.addAll(duplicato);
+            } else {
+                for (GroupForUser wp : duplicato) {
+                    if (wp.getName().toLowerCase(Locale.getDefault())
+                            .contains(charText)) {
+                        mValues.add(wp);
+                    }
+                }
+            }
+            notifyDataSetChanged();
+        }
+
+    }
 }
