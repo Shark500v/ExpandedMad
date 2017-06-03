360f11a285b5d29c5b07f384ffb67f88eeec6414
Shark500v <francesco.pipito@gmail.com>
2017-06-03 20:29:41 +0200

    bugs fixed

--- app/src/main/java/com/polito/madinblack/expandedmad/login/Logout.java
+++ app/src/main/java/com/polito/madinblack/expandedmad/login/Logout.java
@@ -5,9 +5,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -95,5 +99,10 @@ public class Logout extends DialogFragment implements
         Toast.makeText(getActivity(), "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 
-
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setCancelable(false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
